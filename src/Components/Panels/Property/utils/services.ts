238356aae70075b8b54623ed9f78ce3172273381
justingreco <justin.greco@gmail.com>
2023-03-06 10:36:24 -0500

    changing to async await

--- src/Components/Panels/Property/utils/services.ts
+++ src/Components/Panels/Property/utils/services.ts
@@ -1,58 +1,52 @@
-import Graphic from "@arcgis/core/Graphic";
-import MapView from "@arcgis/core/views/MapView";
-import * as geometryEngine from "@arcgis/core/geometry/geometryEngine";
-import WebMap from "@arcgis/core/WebMap";
+import Graphic from '@arcgis/core/Graphic';
+import MapView from '@arcgis/core/views/MapView';
+import * as geometryEngine from '@arcgis/core/geometry/geometryEngine';
+import WebMap from '@arcgis/core/WebMap';
 function loadWebmap(serviceLayers: any[]): Promise<__esri.Layer[]> {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     const map = new WebMap({
       portalItem: {
-        id: "95092428774c4b1fb6a3b6f5fed9fbc4",
+        id: '95092428774c4b1fb6a3b6f5fed9fbc4',
       },
     });
 
-    map.load().then(() => {
-      const layers = map.allLayers.filter((layer: __esri.Layer) => {
-        return serviceLayers.includes(layer.title);
-      });
-      resolve(layers.toArray());
+    await map.load();
+    const layers = map.allLayers.filter((layer: __esri.Layer) => {
+      return serviceLayers.includes(layer.title);
     });
+    resolve(layers.toArray());
   });
 }
-function getServiceData(
+async function getServiceData(
   layers: any[],
   graphic: Graphic,
   setFeatures: Function,
   setSearching: Function,
-  promises: Promise<__esri.FeatureSet>[]
+  promises: Promise<__esri.FeatureSet>[],
 ) {
   layers?.forEach((layer) => {
-    if (layer.type === "feature") {
+    if (layer.type === 'feature') {
       promises.push(
         (layer as __esri.FeatureLayer).queryFeatures({
-          geometry: geometryEngine.geodesicBuffer(
-            (graphic as __esri.Graphic).geometry,
-            -5,
-            9002
-          ) as __esri.Geometry,
-          outFields: ["*"],
+          geometry: geometryEngine.geodesicBuffer((graphic as __esri.Graphic).geometry, -5, 9002) as __esri.Geometry,
+          outFields: ['*'],
           returnGeometry: true,
-        })
+        }),
       );
     }
   });
   const list: Graphic[] = [];
   if (promises.length) {
-    return Promise.all(promises).then((results) => {
-      results.forEach((result) => {
-        if (result.features.length) {
-          result.features.forEach((feature: __esri.Graphic) => {
-            list.push(feature);
-          });
-        }
-      });
-      setSearching(false);
-      setFeatures(list);
+    const results = await Promise.all(promises);
+    results.forEach((result) => {
+      if (result.features.length) {
+        result.features.forEach((feature: __esri.Graphic) => {
+          list.push(feature);
+        });
+      }
     });
+    setSearching(false);
+    setFeatures(list);
   }
 }
 export function getServices(
@@ -61,17 +55,13 @@ export function getServices(
   view: MapView,
   graphic: Graphic,
   setFeatures: Function,
-  setSearching: Function
+  setSearching: Function,
 ) {
-  requestAnimationFrame(() => {
-    if (
-      (e.target as HTMLCalciteAccordionItemElement).hasAttribute("expanded")
-    ) {
+  requestAnimationFrame(async () => {
+    if ((e.target as HTMLCalciteAccordionItemElement).hasAttribute('expanded')) {
       setSearching(true);
       setFeatures([]);
-      const title = (e.target as HTMLCalciteAccordionItemElement).getAttribute(
-        "item-title"
-      );
+      const title = (e.target as HTMLCalciteAccordionItemElement).getAttribute('item-title');
       const service = services.find((s) => {
         return s.group.title === title;
       });
@@ -83,23 +73,10 @@ export function getServices(
         });
 
         if (layers.length !== service.group.layers.length) {
-          loadWebmap(service.group.layers).then((layers: any[]) => {
-            getServiceData(
-              layers,
-              graphic,
-              setFeatures,
-              setSearching,
-              promises
-            );
-          });
+          const layers = await loadWebmap(service.group.layers);
+          getServiceData(layers, graphic, setFeatures, setSearching, promises);
         } else {
-          getServiceData(
-            layers.toArray(),
-            graphic,
-            setFeatures,
-            setSearching,
-            promises
-          );
+          getServiceData(layers.toArray(), graphic, setFeatures, setSearching, promises);
         }
       }
     }
