0998835dc5c2fa1c4938a7867e64d8190c94b06c
justingreco <justin.greco@gmail.com>
2023-03-08 12:01:13 -0500

    moved alert to its own component

---
+++ src/Components/Shell/utils/AppTips.tsx
@@ -0,0 +1,45 @@
+import React, { lazy, Suspense, useEffect, useRef, useState } from 'react';
+import { CalciteTip, CalciteTipGroup, CalciteTipManager } from '@esri/calcite-components-react';
+interface AppTip {
+  title: string;
+  text: string;
+}
+interface AppTipGroup {
+  title: string;
+  tips: AppTip[];
+}
+interface Props {
+  tips: AppTipGroup;
+}
+function AppTips(props: Props) {
+  const manager = useRef<HTMLCalciteTipManagerElement>(null);
+  const [tips, setTips] = useState<AppTipGroup>();
+  useEffect(() => {
+    setTips(props.tips);
+    setTimeout(() => {
+      if (manager.current) {
+        console.log(props.tips?.title);
+        if (manager.current.shadowRoot) {
+          const heading = manager.current.shadowRoot.querySelector('.heading');
+          if (heading) {
+            heading.innerHTML = props.tips?.title;
+          }
+        }
+      }
+    }, 100);
+  }, [props.tips]);
+  return (
+    <CalciteTipManager ref={manager} closed={tips === undefined ? true : undefined}>
+      <CalciteTipGroup>
+        {props.tips?.tips.map((tip: any) => {
+          return (
+            <CalciteTip heading={tip.title} key={tip.title}>
+              {tip.text}
+            </CalciteTip>
+          );
+        })}
+      </CalciteTipGroup>
+    </CalciteTipManager>
+  );
+}
+export default AppTips;
