8eb806e8989bbc633b5cae7976defda7d22e6e2b
justingreco <justin.greco@gmail.com>
2024-09-19 15:21:39 -0400

    changed to safe functions

--- src/components/WebMap/utils/WebMapProps.ts
+++ src/components/WebMap/utils/WebMapProps.ts
@@ -1,8 +1,10 @@
+import { Alert } from "../../Shell/utils/alert";
+
 export interface WebMapProps {
   mapId: string;
   properties: __esri.Graphic[];
-  mapViewSet: Function;
-  geometrySet: Function;
-  widgetActivated: Function;
-  alertSet?: Function | undefined;
+  mapViewSet: (mapView: __esri.MapView) => void;
+  geometrySet: (geometry: __esri.Geometry) => void;
+  widgetActivated: (view: __esri.MapView, setActiveTool: (activeTool: string) => void) => void;
+  alertSet?: (alert: Alert) => void;
 }
